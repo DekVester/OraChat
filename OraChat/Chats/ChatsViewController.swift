0f59ee12ea8396822fce1517625ccf7908911c90
Igor Vasilev <ivasilev@productiveedge.com>
2016-11-11 20:30:29 +0100

    ChatsViewController is added

---
+++ OraChat/Chats/ChatsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ChatsViewController.swift
+//  OraChat
+//
+//  Created by Igor Vasilev on 11/11/16.
+//  Copyright © 2016 Productive Edge. All rights reserved.
+//
+
+import UIKit
+
+class ChatsViewController: UITableViewController {
+}
